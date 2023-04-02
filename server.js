a7f04048b612fc40548d12b27a6f5b56f70119bf
Snowman271 <deanbenzmiller@yahoo.com>
2023-04-02 16:41:20 -0700

    Completed File Structure

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
